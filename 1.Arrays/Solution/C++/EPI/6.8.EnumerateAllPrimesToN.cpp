6fc5315a292aacbc9ec97481cdde6cc42e7c1acc
lightertu <ruitu.login@gmail.com>
2016-12-20 13:15:58 -0800

    6.8 smart solution

--- 1.Arrays/Solution/C++/EPI/6.8.EnumerateAllPrimesToN.cpp
+++ 1.Arrays/Solution/C++/EPI/6.8.EnumerateAllPrimesToN.cpp
@@ -5,8 +5,7 @@ using namespace std;
 /* brute force solution check if the current number of 
  * a multiple of primes*/
 vector<int> AllPrimes(int n) {
-    vector<int> primes = {1, 2};
-
+    vector<int> primes = {1, 2}; 
     if (n == 0) { return vector<int>{ }; }
     if (n == 1) { return vector<int>{ 1 }; }
     if (n == 2) { return primes; }
@@ -27,4 +26,30 @@ vector<int> AllPrimes(int n) {
     }
 
     return primes;
+}
+
+/* smart solution */
+vector<int> AllPrimesMemorized(int n) {
+    vector<bool> T(n, true);
+    vector<int> primes = { 1 };
+
+    /* edge cases */
+    if (n == 0) { return vector<int>{ }; }
+    if (n == 1) { return primes; }
+
+    /* base case 1 and 2 are primes*/
+    for (int i = 2; i < n; i++) {
+        if (T[i]) {
+            primes.push_back(i);
+            for (int j = 1; i * j < n; ++j) {
+                T[i * j] = false;
+            }
+        }
+    }
+
+    return primes;
+}
+
+int main() {
+    print_vector(AllPrimesMemorized(100));
 }
