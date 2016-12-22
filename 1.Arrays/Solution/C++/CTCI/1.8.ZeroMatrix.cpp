63726e36f5956bf1bbda91bd849a8fb5c1118be9
lightertu <ruitu.login@gmail.com>
2016-12-22 13:50:36 -0800

    fixing bugs for 1.8 ctci

--- 1.Arrays/Solution/C++/CTCI/1.8.ZeroMatrix.cpp
+++ 1.Arrays/Solution/C++/CTCI/1.8.ZeroMatrix.cpp
@@ -7,7 +7,26 @@ void ZeroMatrixInPlace(vector<vector<int>>& matrix) {
     /* using top row and the left most column to memoize whichs 
      * rows and columns should be set to zero */
     int width = matrix[0].size(),
-        height = matrix.size();
+        height = matrix.size(),
+        first_row = 0,
+        first_col = 0;
+
+    bool first_row_zero = false,
+         first_col_zero = false;
+    
+    /* check if the first row should be emptied */
+    for (int col = 0; col < width && !first_row_zero; ++col) {
+        if (matrix[first_row][col] == 0) {
+            first_row_zero = true;
+        }
+    }
+
+    /* check if the first column should be emptied */
+    for (int row = 0; row < width && !first_col_zero; ++row) {
+        if (matrix[row][first_col] == 0) {
+            first_col_zero = true;
+        }
+    }
 
     /* memorize the zero rows and zero columns */
     for (int row = 0; row < height; ++row) {
@@ -19,32 +38,42 @@ void ZeroMatrixInPlace(vector<vector<int>>& matrix) {
         }
     }
     
-    /* set zero rows to zero */
-    for (int row = 0; row < height; ++row) {
-        int first_col = 0;
+    /* set zero rows to zero 
+     * iterating starting from the second row since we use the first row
+     * to memoize the zero columns*/
+    for (int row = 1; row < height; ++row) {
         if (matrix[row][first_col] == 0) {
             fill(matrix[row].begin(), matrix[row].end(), 0);
         }
     }
     
     /* set columns to zero */
-    for (int col = 0; col < width; ++col) {
-        int first_row = 0;
+    for (int col = 1; col < width; ++col) {
         if (matrix[first_row][col] == 0) {
             for (int row = 0; row < height; ++row) {
                 matrix[row][col] = 0;
             }
         }
     }
+
+    if (first_row_zero) {
+        fill(matrix[first_row].begin(), matrix[first_col].end(), 0);
+    }
+
+    if (first_col_zero) {
+        for (int row = 0; row < height; ++row) {
+            matrix[row][first_col] = 0;
+        }
+    }
 }
 
 int main(void) {
     vector<vector<int>> matrix = {
-        {1,1,1,1,1},
-        {1,0,1,1,1},
-        {1,0,0,1,1},
-        {1,1,1,0,1},
-        {1,1,1,1,1}
+        {0,1,1,1},
+	    {4,3,1,4},
+	    {1,1,1,4},
+	    {0,2,1,3},
+	    {1,0,0,1} 
     };
 
     print_matrix(matrix);
