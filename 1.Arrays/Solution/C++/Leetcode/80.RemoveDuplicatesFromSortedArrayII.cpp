639ca783ba936f7aca34295c64f5300aa2257754
lightertu <ruitu.login@gmail.com>
2016-12-27 10:02:44 -0800

    leetcode 80 unfinished

---
+++ 1.Arrays/Solution/C++/Leetcode/80.RemoveDuplicatesFromSortedArrayII.cpp
@@ -0,0 +1,17 @@
+#include "../Debug.h"
+
+using namespace std; 
+
+int RemoveDuplicates(vector<int>& A) {
+
+}
+
+int main(void) {
+                            /* w */  
+    vector<int> A = { 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8};
+                         /* i */
+    RemoveDuplicates(A);
+    print_vector(A);
+
+    return 0;
+}
