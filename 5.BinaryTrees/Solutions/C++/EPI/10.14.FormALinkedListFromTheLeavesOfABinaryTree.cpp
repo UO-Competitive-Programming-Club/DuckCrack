fa968a091d43c465232375cdf9ca919952a7a599
lightertu <ruitu.login@gmail.com>
2017-02-01 10:13:36 -0800

    changed vector to list

--- 5.BinaryTrees/Solutions/C++/EPI/10.14.FormALinkedListFromTheLeavesOfABinaryTree.cpp
+++ 5.BinaryTrees/Solutions/C++/EPI/10.14.FormALinkedListFromTheLeavesOfABinaryTree.cpp
@@ -1,15 +1,15 @@
 #include "../Debug.h" 
 using namespace std;
 
-void LeavesLinkedListHelper(TreeNode* root, vector<TreeNode*> result);
+void LeavesLinkedListHelper(TreeNode* root, list<TreeNode*> result);
 
-vector<TreeNode*> LeavesLinkedList(TreeNode* root) {
-    vector<TreeNode*> result;
+list<TreeNode*> LeavesLinkedList(TreeNode* root) {
+    list<TreeNode*> result;
     LeavesLinkedListHelper(root, result);
     return result;
 }
 
-void LeavesLinkedListHelper(TreeNode* root, vector<TreeNode*> result) {
+void LeavesLinkedListHelper(TreeNode* root, list<TreeNode*> result) {
     if (root) {
         if (!root->left && root->right) {
             result.emplace_back(root);
