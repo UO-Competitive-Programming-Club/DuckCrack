16945e7bd5cb70dbb0f0ddea4de2b16bcf14aa22
lightertu <ruitu.login@gmail.com>
2017-02-14 22:42:20 -0800

    some changes

---
+++ 5.BinaryTrees/Solutions/C++/EPI/10.16.ComputeTheRightSiblingTree.cpp
@@ -0,0 +1,18 @@
+#include "../Debug.h"
+using namespace std;
+
+struct TreeLinkNode {
+	TreeLinkNode *left;
+	TreeLinkNode *right;
+	TreeLinkNode *next;
+};
+
+void connect(TreeLinkNode *root) {
+    auto iter = root;
+    while (iter) {
+        auto level_iter = iter;
+
+    }
+}
+
+
