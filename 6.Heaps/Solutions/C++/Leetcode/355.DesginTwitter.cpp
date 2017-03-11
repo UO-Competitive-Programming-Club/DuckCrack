d7fe47d1ff2f3b656fa9bf59c74e7d8ff7fe5d2d
lightertu <ruitu.login@gmail.com>
2017-03-11 13:04:22 -0800

    leecode 335 ac

--- 6.Heaps/Solutions/C++/Leetcode/355.DesginTwitter.cpp
+++ 6.Heaps/Solutions/C++/Leetcode/355.DesginTwitter.cpp
@@ -1,16 +1,19 @@
 #include "../Debug.h"
 using namespace std;
 
+
+/* edge cases: 
+ *      you cannot follow yourself
+ * */
+
 class Twitter {
 public:
     /** Initialize your data structure here. */
     struct Tweet {
-        int user_id; 
         int tweet_id;
         int timestamp;
 
-        Tweet(int user_id, int tweet_id, int timestamp ) {
-            this->user_id = user_id;
+        Tweet(int tweet_id, int timestamp ) {
             this->tweet_id = tweet_id;
             this->timestamp = timestamp;
         }
@@ -19,28 +22,16 @@ public:
     struct User {
         int id;
         unordered_set<int> followers;
-        unordered_set<int> following;
-        unordered_map<int, Tweet> tweets;
-        list<Tweet> lastest_tweets;
+        unordered_set<int> followings;
+        list<Tweet> tweets;
 
         User(int user_id) : id(user_id) { };
 
         void post(int tweet_id, int timestamp) {
-            Tweet new_tweet(this->id, tweet_id, timestamp);
-            if (tweets.find(tweet_id) != tweets.end()) {
-                tweets.insert({tweet_id,  new_tweet});
-            } else {
-                // throw exception
-            }
-
-            if (lastest_tweets.size() >= 10) {
-                lastest_tweets.pop_back();
-            }
-
-            lastest_tweets.push_front(new_tweet);
+            Tweet new_tweet(tweet_id, timestamp);
+            tweets.push_front(new_tweet);
         }
     };
-
 
     unordered_map<int, User> userDB;
     int time;
@@ -48,27 +39,23 @@ public:
     Twitter() {
         this->time = 0;
     }
-    
+
     /** Compose a new tweet. */
     void postTweet(int userId, int tweetId) {
-        if (userDB.find(userId) != userDB.end()) {
+
+        if (userDB.find(userId) == userDB.end()) {
             User new_user(userId);
             userDB.insert({userId, new_user});
-            new_user.post(tweetId, time);
-        } else {
-            User& user = userDB.at(userId);
-            user.post(tweetId, time);
+            //cout << "here" << endl;
         }
 
+        User& user = userDB.at(userId);
+        user.post(tweetId, time);
         time++;
     }
-    
-    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the 
-     * news feed must be posted by users who the user followed or by the user herself. 
-     * Tweets must be ordered from most recent to least recent. */
 
     vector<int> getNewsFeed(int userId) {
-        vector<int> result(10); 
+        vector<int> result;
         if (userDB.find(userId) == userDB.end()) {
             return result;
         }
@@ -77,19 +64,27 @@ public:
             list<Tweet>::const_iterator iter;
             list<Tweet>::const_iterator end;
 
-            bool operator>(const Entry& that) const {
-                return this->iter->timestamp > that.iter->timestamp;
+            bool operator<(const Entry& that) const {
+                return this->iter->timestamp < that.iter->timestamp;
             }
         };
 
-        priority_queue<Entry, vector<Entry>, greater<Entry> > max_heap;
+        priority_queue<Entry, vector<Entry>, less<Entry> > max_heap;
         User& user = userDB.at(userId);
-
         /* initalization */
-        for (auto iter = user.followers.cbegin(); iter != user.followers.cend(); ++iter) {
+                    
+                    
+        if (!user.tweets.empty()) {
+            max_heap.emplace(Entry{ user.tweets.cbegin(), user.tweets.cend() });
+        }
+        
+        for (auto iter = user.followings.cbegin(); iter != user.followings.cend(); ++iter) {
             int followee_id = *iter;
             User& followee = userDB.at(followee_id);
-            max_heap.emplace(Entry{ followee.lastest_tweets.cbegin(), followee.lastest_tweets.cend() });
+            
+            if (!followee.tweets.empty()) {
+                max_heap.emplace(Entry{ followee.tweets.cbegin(), followee.tweets.cend() });
+            }
         }
 
         for (int i = 0; i < 10 && !max_heap.empty(); i++) {
@@ -105,30 +100,58 @@ public:
 
         return result;
     }
-    
+
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
     void follow(int followerId, int followeeId) {
-        if (userDB.find(followerId) != userDB.end() && 
-            userDB.find(followeeId) != userDB.end()) {
-
+        
+        if (userDB.find(followerId) == userDB.end()) {
+            User new_user(followerId);
+            userDB.insert({followerId, new_user});
+        }
+        
+        if (userDB.find(followeeId) == userDB.end()) {
+            User new_user(followeeId);
+            userDB.insert({followeeId, new_user});
+        }
+        
+        if (followerId != followeeId) {
             User& follower = userDB.at(followerId);
             User& followee = userDB.at(followeeId);
-            
-            follower.following.insert(followeeId);
-            followee.followers.insert(followerId);
-        } 
-    }
     
+            follower.followings.insert(followeeId);
+            followee.followers.insert(followerId);
+        }
+    }
+
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     void unfollow(int followerId, int followeeId) {
-        if (userDB.find(followerId) != userDB.end() && 
-            userDB.find(followeeId) != userDB.end()) {
-
+        
+        if (userDB.find(followerId) == userDB.end()) {
+            User new_user(followerId);
+            userDB.insert({followerId, new_user});
+        }
+        
+        if (userDB.find(followeeId) == userDB.end()) {
+            User new_user(followeeId);
+            userDB.insert({followeeId, new_user});
+        }
+        
+        if (followerId != followeeId) {
             User& follower = userDB.at(followerId);
             User& followee = userDB.at(followeeId);
-            
-            follower.following.erase(followeeId);
+    
+            follower.followings.erase(followeeId);
             followee.followers.erase(followerId);
-        } 
+        }
     }
 };
+
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * vector<int> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
